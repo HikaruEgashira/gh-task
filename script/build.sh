7d30528a1c8da1dfd94b982348b1d7aafc085c9f
HikaruEgashira <account@egahika.dev>
2026-03-22 23:21:49 +0900

    Remove unused script directory

--- script/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Build gh-task for the current platform
-# Used by: gh extension install HikaruEgashira/gh-task
-
-zig build -Doptimize=ReleaseSafe
-cp zig-out/bin/gh-task .
-echo "Built gh-task successfully"
